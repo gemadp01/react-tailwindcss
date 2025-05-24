412b5c0814b0d4965069b81e75a042db0058e2fc
Gema Dodi Pranata <gemadp01@gmail.com>
2025-05-24 18:05:18 +0700

    feat: penulisan inline tailwindcss dengan curly braces

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,21 @@ function App() {
   return (
     <>
       <div className="flex items-center justify-center min-h-screen">
-        <button className="py-2 px-4 rounded bg-blue-500 hover:bg-blue-600 shadow-xl shadow-blue-300 hover:shadow-none text-white font-semibold transition">
+        <button
+          className={`
+            py-2 
+            px-4 
+            rounded 
+            bg-blue-500 
+            hover:bg-blue-600 
+            shadow-xl 
+            shadow-blue-300 
+            hover:shadow-none 
+            text-white 
+            font-semibold 
+            transition
+          `}
+        >
           Button
         </button>
       </div>
